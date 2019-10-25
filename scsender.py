f2cd5c233366075c5dedcdd03f4049e9656f24d9
ceynri <347670115@qq.com>
2019-10-25 21:40:56 +0800

    fix: 修复永远显示“数据未更新”的bug

--- scsender.py
+++ scsender.py
@@ -13,16 +13,17 @@ def handle(data: list, describe: str):
     # 标题
     text = ''
     cur_date = time.strftime("%Y-%m-%d", time.localtime())
-    if data[-2]['date'] is cur_date:
+    if data[-1]['date'] == cur_date:
         text = '昨日用电{:.2f}度，今日可用{:.2f}度'.format(
             data[-2]['cost'], data[-1]['rest'])
     else:
-        text = '数据未更新(σ｀д′)σ'
+        text = '数据未更新(￣_￣|||)'
 
     # 详细内容的文本
 
     # 表头
     desp = describe + '\n\n'
+    # 出于Sever酱的markdown表格样式问题，首行表格空格为全角空格
     desp += '|　日期　|　当日用电　|　可用电量　|　当日充电　|\n' + \
             '| :---: | :------: | :------: | :------: |\n'
 
@@ -31,13 +32,12 @@ def handle(data: list, describe: str):
         for datum in line:
             # float数据控制小数点为两位
             if isinstance(line[datum], float):
-                # 出于Sever酱的markdown表格样式问题，数据后面尾随全角空格一个
                 desp += '| {:.2f} '.format(line[datum])
             else:
                 desp += '| {} '.format(line[datum])
         desp += '|\n'
 
-    print(desp)  # 测试用
+    # print(desp)  # 测试用
 
     data = {
         'text': text,
